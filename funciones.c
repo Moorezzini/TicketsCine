f5dd96d4021b2407d4b7ac228c4ad64641fd1ff1
carlos.guaita <carlos.guaita@udla.edu.ec>
2024-06-17 14:25:59 -0500

    inicio

---
+++ funciones.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "funciones.h"
+#include <string.h>
+
+void ingresarCliente(char clientes[][2][40]){
+   
+    for (int i = 0; i < 5; i++)
+    {
+        if(strcmp(clientes[i][0],"")==0){
+            printf("Ingrese nombre del nuevo cliente: ");
+            scanf("%s",clientes[i][0]);
+            printf("Ingrese cedula del nuevo cliente: ");
+            scanf("%s",clientes[i][1]);
+            break;
+        }
+    } 
+    imprimirClientes(clientes);
+}
+
+void imprimirClientes(char clientes[][2][40]){
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%s\t\t%s\n",clientes[i][0],clientes[i][1]);
+    }   
+}
---
+++ funciones.h
@@ -0,0 +1,2 @@
+void ingresarCliente(char clientes[][2][40]);
+void imprimirClientes(char clientes[][2][40]);
